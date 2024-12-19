560312e0df92de69310a5cf6b830a514a5c39057
janis <janis.ellerbrock@gmail.com>
2024-12-19 16:44:56 +0100

    add new supported chains section

--- src/lib/data/chainData.ts
+++ src/lib/data/chainData.ts
@@ -1,41 +1,49 @@
 export const chainData = [
   {
-    logo: '/chains/near.svg',
+    logo: '/chains/new_near.svg',
+    name: 'Near',
     scale: 0.8,
     enabled: false,
   },
   {
-    logo: '/chains/eth.svg',
+    logo: '/chains/New_eth.svg',
+    name: 'Ethereum',
     scale: 0.9,
     enabled: false,
   },
   {
-    logo: '/chains/base.svg',
+    logo: '/chains/new_base.svg',
+    name: 'Base',
     scale: 0.9,
     enabled: false,
   },
-  {
+  /*   {
     logo: '/chains/bnb.svg',
+    name: 'BNB',
+    scale: 0.9,
+    enabled: false,
+  }, */
+  {
+    logo: '/chains/new_polygon.svg',
+    name: 'Polygon',
     scale: 0.9,
     enabled: false,
   },
-  {
-    logo: '/chains/polygon.svg',
-    scale: 0.9,
-    enabled: false,
-  },
-  {
+  /*   {
     logo: '/chains/gno.svg',
+    name: 'Gnosis',
     scale: 0.9,
     enabled: false,
-  },
-  {
+  }, */
+  /*   {
     logo: '/chains/arbitrum.svg',
+    name: 'Arbitrum',
     scale: 0.9,
     enabled: false,
-  },
+  }, */
   {
-    logo: '/chains/op.svg',
+    logo: '/chains/new_op.svg',
+    name: 'Optimism',
     scale: 0.9,
     enabled: false,
   },
