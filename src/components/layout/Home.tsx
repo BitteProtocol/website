2d903093dfd53ca052eb6dcc98d8f9a67fcdf8aa
janis <janis.ellerbrock@gmail.com>
2024-12-19 17:45:58 +0100

    add back VideoSection component and render on home

--- src/components/layout/Home.tsx
+++ src/components/layout/Home.tsx
@@ -7,6 +7,7 @@ import { PartnersSection } from './PartnersSection';
 import TextSection from './TextSection';
 import { ProductCardsSection } from './ProductCardsSection';
 import { SupportedChainsSection } from './SupportedChainsSection';
+import { MB_URL } from '@/lib/url';
 import {
   dropCardData,
   newsCardData,
@@ -14,6 +15,7 @@ import {
 } from '@/lib/data/exampleCardData';
 import { AgentSection } from './AgentSection';
 import { Filters, RegistryData } from '@/lib/types/agent.types';
+import VideoSection from './VideoSection';
 
 export type AgentData = {
   agents: RegistryData[];
@@ -21,17 +23,7 @@ export type AgentData = {
   filters: Filters[];
 };
 
-const paymasterSection = {
-  title: 'Any API can become an AI agent',
-  subHeader:
-    'Explore existing agents or create your own. We make it simple to infuse AI capabilities into any API.',
-  factTitle: '',
-  fact: '',
-  isDisabled: false,
-  noSpacing: true,
-};
-
-const crossSection = {
+const headerTextSection = {
   title: 'Blockchain empowered by AI Agents',
   subHeader:
     'Explore existing agents or create your own. Any API can easily become an agent.',
@@ -41,20 +33,47 @@ const crossSection = {
   noSpacing: true,
 };
 
+const paymasterSection = {
+  thumb: '/video/paymaster-thumb.jpg',
+  src: '/video/paymaster.mp4',
+  title: 'Paymaster',
+  subHeader:
+    'Fund gasless transactions for your community on any NEAR smart contract and enable or disable specific functions.',
+  factTitle: 'Sponsored Relays',
+  fact: '+250k',
+  btnTitle: 'Sponsor Now',
+  btnUrl: MB_URL.PAYMASTER,
+  isDisabled: false,
+};
+
+const crossSection = {
+  thumb: '/video/cross-thumb.jpg',
+  src: '/video/cross-chain.mp4',
+  title: 'Universal Accounts with Chain Abstraction',
+  subHeader: "One account to rule them all, with ERC-4337 Safe's on EVMs.",
+  factTitle: '',
+  fact: '',
+  btnTitle: 'Try Now',
+  btnUrl: MB_URL.BITTE_WALLET,
+  isDisabled: false,
+};
+
 export const HomeComponent = ({ agentData }: { agentData: AgentData }) => {
   return (
     <>
       <SupportedChainsSection />
-      <TextSection {...crossSection} />
+      <TextSection {...headerTextSection} />
       <AgentSection agentData={agentData} />
       <ProductCardsSection data={productCardsData} />
 
       <ExamplesSection data={dropCardData} isVideo={false} />
-      <TextSection {...paymasterSection} />
       <ExamplesSection data={newsCardData} isVideo={false} />
       <ExamplesSection data={videosCardData} isVideo={true} />
 
+      <VideoSection {...crossSection} />
+
       <NumbersSection />
+      <VideoSection {...paymasterSection} />
       <PartnersSection />
     </>
   );
