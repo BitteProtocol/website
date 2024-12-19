7a0b51eff025d9d17af12153624bf0f495825da7
janis <janis.ellerbrock@gmail.com>
2024-12-19 18:27:05 +0100

    add padding to chain section

--- src/components/layout/SupportedChainsSection.tsx
+++ src/components/layout/SupportedChainsSection.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 export const SupportedChainsSection = () => {
   return (
-    <section className='relative mt-12 md:mt-24 mb-16 md:mb-32'>
+    <section className='relative mt-12 md:mt-24 mb-16 md:mb-32 px-5'>
       <p
         className={`${fira.className} text-sm text-mb-gray-300 font-normal text-center uppercase mb-6`}
       >
