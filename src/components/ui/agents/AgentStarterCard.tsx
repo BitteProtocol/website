5e141689416e47d0c3fab8338ffb098f76add36e
janis <janis.ellerbrock@gmail.com>
2024-12-23 15:07:53 +0100

    add link to starter card

--- src/components/ui/agents/AgentStarterCard.tsx
+++ src/components/ui/agents/AgentStarterCard.tsx
@@ -2,21 +2,24 @@
 
 import { Card, CardContent } from '../card';
 import { ArrowRightCircle } from 'lucide-react';
+import Link from 'next/link';
 
 export const AgentStarterCard = (): JSX.Element => {
   return (
-    <Card className='border border-[#CAFFF280] w-full md:w-[402px]'>
-      <CardContent className='p-4 flex items-center justify-between'>
-        <div>
-          <p className='text-[#CAFFF2] text-lg font-bold mb-1.5'>
-            Agent Starter
-          </p>
-          <p className='text-[#CBD5E1]'>
-            Create your own AI Agents in minutes.
-          </p>
-        </div>
-        <ArrowRightCircle size={32} className='' color='#CAFFF2' />
-      </CardContent>
-    </Card>
+    <Link href='/registry/agent-starter'>
+      <Card className='border border-[#CAFFF280] w-full md:w-[402px]'>
+        <CardContent className='p-4 flex items-center justify-between'>
+          <div>
+            <p className='text-[#CAFFF2] text-lg font-bold mb-1.5'>
+              Agent Starter
+            </p>
+            <p className='text-[#CBD5E1]'>
+              Create your own AI Agents in minutes.
+            </p>
+          </div>
+          <ArrowRightCircle size={32} color='#CAFFF2' />
+        </CardContent>
+      </Card>
+    </Link>
   );
 };
