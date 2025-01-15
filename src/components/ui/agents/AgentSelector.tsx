972669ceedee68a3a695ef1d5a5ce860f94448b9
janis <janis.ellerbrock@gmail.com>
2025-01-15 16:05:40 +0000

    add agent container

--- src/components/ui/agents/AgentSelector.tsx
+++ src/components/ui/agents/AgentSelector.tsx
@@ -14,17 +14,20 @@ export const AgentSelector = ({
 }) => {
   return (
     <div
-      className='flex flex-row lg:flex-col gap-4 items-center overflow-x-auto disable-scrollbars h-full -mx-6 lg:mx-0 lg:pl-0 pl-6'
+      className='flex flex-row lg:flex-col gap-4 items-center overflow-x-auto disable-scrollbars h-full -mx-6 lg:mx-0 lg:pl-0 pl-6 bg-[#18181A] border border-[#334155] py-3 rounded-md'
       style={{ scrollBehavior: 'smooth', whiteSpace: 'nowrap' }}
     >
+      <div className='pt-2 pb-5 px-3 border-b border-[#334155] w-full'>
+        <p className='text-[#F8FAFC] font-semibold text-left'>Agents</p>
+      </div>
       {agentData?.map((data, i) => (
         <Card
           key={`agents-${i}`}
-          className='last:mr-6 lg:last:mr-0 min-w-[307px] h-[76px] flex items-center bg-[#18181A] cursor-pointer'
+          className='last:mr-6 lg:last:mr-0 min-w-[307px] h-[76px] flex items-center bg-black border-none cursor-pointer'
           onClick={() => onSelectAgent(data)}
         >
           <CardContent
-            className={`text-center p-3 flex items-center gap-3 w-full h-full rounded ${selectedAgent?.id == data.id ? 'bg-[#C084FC33] border border-[#C084FC]' : ''} hover:border hover:border-[#C084FC]`}
+            className={`text-center p-3 flex items-center gap-3 w-full h-full rounded-md ${selectedAgent?.id == data.id ? 'bg-[#C084FC33] border border-[#C084FC]' : ''} hover:border hover:border-[#C084FC]`}
           >
             <div>
               <Image
