105a19a8fa4a51ead401e1f10f469ef9101a02a4
janis <janis.ellerbrock@gmail.com>
2025-01-16 12:33:41 +0000

    fix details of agent selector container and add mobile layout

--- src/components/ui/agents/AgentSelector.tsx
+++ src/components/ui/agents/AgentSelector.tsx
@@ -14,16 +14,16 @@ export const AgentSelector = ({
 }) => {
   return (
     <div
-      className='flex flex-row lg:flex-col gap-4 items-center overflow-x-auto disable-scrollbars h-full -mx-6 lg:mx-0 lg:pl-0 pl-6 bg-[#18181A] border border-[#334155] py-3 rounded-md'
+      className='flex flex-row lg:flex-col gap-4 items-center overflow-x-auto disable-scrollbars h-full lg:bg-[#18181A] lg:border lg:border-[#334155] lg:py-3 lg:rounded-md lg:p-6'
       style={{ scrollBehavior: 'smooth', whiteSpace: 'nowrap' }}
     >
-      <div className='pt-2 pb-5 px-3 border-b border-[#334155] w-full'>
+      <div className='pt-2 pb-5 px-3 border-b border-[#334155] w-full hidden lg:block'>
         <p className='text-[#F8FAFC] font-semibold text-left'>Agents</p>
       </div>
       {agentData?.map((data, i) => (
         <Card
           key={`agents-${i}`}
-          className='last:mr-6 lg:last:mr-0 min-w-[307px] h-[76px] flex items-center bg-black border-none cursor-pointer'
+          className='last:mr-6 lg:last:mr-0 min-w-[219px] lg:min-w-[307px] h-[76px] flex items-center bg-black border-none cursor-pointer'
           onClick={() => onSelectAgent(data)}
         >
           <CardContent
@@ -39,7 +39,9 @@ export const AgentSelector = ({
                 loading='lazy'
               />
             </div>
-            <div className='font-medium text-[#F8FAFC]'>{data?.name}</div>
+            <div className='font-medium text-[#F8FAFC] break-words w-full overflow-hidden text-ellipsis'>
+              {data?.name}
+            </div>
           </CardContent>
         </Card>
       ))}
