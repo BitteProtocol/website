09a19d9177bf6ae21d732c2e86d5571c2788398f
janis <janis.ellerbrock@gmail.com>
2025-01-17 17:14:00 +0000

    fix mobile margin for chat

--- src/components/layout/Hero.tsx
+++ src/components/layout/Hero.tsx
@@ -119,7 +119,7 @@ const Hero = ({ agentData }: { agentData: AgentData }) => {
                 selectedAgent={selectedAgent}
               />
             </div>
-            <div className='w-full h-full'>
+            <div className='lg:w-full h-full -mx-8 lg:-mx-0'>
               <BitteAiChat
                 options={{
                   agentImage: selectedAgent?.coverImage,
