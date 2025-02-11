0c16832b10da32fcf3efc357ef76d44c53731de8
janis <janis.ellerbrock@gmail.com>
2025-02-11 12:06:19 +0000

    remove grid

--- src/components/layout/AgentsContent.tsx
+++ src/components/layout/AgentsContent.tsx
@@ -20,11 +20,6 @@ const AgentContent = ({
 }) => {
   return (
     <SidebarLayout>
-      <div className='grid auto-rows-min gap-4 md:grid-cols-3'>
-        <div className='aspect-video rounded-xl bg-muted/50' />
-        <div className='aspect-video rounded-xl bg-muted/50' />
-        <div className='aspect-video rounded-xl bg-muted/50' />
-      </div>
       <div className='min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min' />
       <div className='relative z-30'>
         <AllAgentsWithNoSSR
