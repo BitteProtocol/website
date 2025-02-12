d63c783f54427f7a054700ea2da1433ddfbf41f1
ruisantiago <ruisantiagomr@gmail.com>
2025-02-12 12:28:23 +0000

    lin

--- src/components/layout/Hero.tsx
+++ src/components/layout/Hero.tsx
@@ -6,26 +6,21 @@ import { useEffect, useState } from 'react';
 import { useAccount } from 'wagmi';
 
 import { AgentData } from '@/components/layout/Home';
-import AgentSelector from '@/components/ui/agents/AgentSelector';
-import Filters from '@/components/ui/agents/Filters';
-import { Filters as AgentFilters, RegistryData } from '@/lib/types/agent.types';
+import { RegistryData } from '@/lib/types/agent.types';
 import { MB_URL } from '@/lib/url';
-import { cn } from '@/lib/utils';
-import { filterHandler } from '@/lib/utils/filters';
 import Link from 'next/link';
 import { Button } from '../ui/button';
-import AiChat from './AiChat';
 
 const Hero = ({ agentData }: { agentData: AgentData }) => {
   const [selectedAgent, setSelectedAgent] = useState<RegistryData | null>(null);
-  const [selectedFilters, setSelectedFilters] = useState<AgentFilters[]>([]);
-
+  /*   const [selectedFilters, setSelectedFilters] = useState<AgentFilters[]>([]);
+   */
   const { isConnected } = useBitteWallet();
 
   const { isConnected: isEvmConnected } = useAccount();
   const isWalletDisconnected = !isConnected && !isEvmConnected;
 
-  const filteredAgents = selectedFilters?.length
+  /*   const filteredAgents = selectedFilters?.length
     ? agentData.agents.filter((agent) => {
         if (!agent) return false;
 
@@ -42,7 +37,7 @@ const Hero = ({ agentData }: { agentData: AgentData }) => {
     setSelectedFilters((prevFilters) =>
       filterHandler(prevFilters, value, label)
     );
-  };
+  }; */
 
   useEffect(() => {
     if (agentData.agents.length) {
@@ -68,25 +63,16 @@ const Hero = ({ agentData }: { agentData: AgentData }) => {
   return (
     <section className='w-full'>
       <div className='relative'>
-        {isWalletDisconnected ? (
-          <video
-            autoPlay
-            loop
-            playsInline
-            muted
-            className='absolute w-screen h-full object-cover border-b border-[#313E52] opacity-20'
-          >
-            <source src='/video/brains.mp4' type='video/mp4' />
-          </video>
-        ) : null}
-        <div
-          className={cn(
-            'flex justify-center flex-col text-center items-center px-8 py-28',
-            isWalletDisconnected
-              ? 'pt-14 md:pt-12 lg:pt-12 xl:pt-24 2xl:pt-40'
-              : 'pt-4'
-          )}
+        <video
+          autoPlay
+          loop
+          playsInline
+          muted
+          className='absolute w-screen h-full object-cover border-b border-[#313E52] opacity-20'
         >
+          <source src='/video/brains.mp4' type='video/mp4' />
+        </video>
+        <div className='flex justify-center flex-col text-center items-center px-8 py-28 pt-14 md:pt-12 lg:pt-12 xl:pt-24 2xl:pt-40'>
           {isWalletDisconnected ? (
             <div className='z-10 md:pointer-events-none'>
               <p className='font-semibold text-white text-[32px] md:text-[40px] leading-tight mx-auto '>
@@ -95,7 +81,7 @@ const Hero = ({ agentData }: { agentData: AgentData }) => {
             </div>
           ) : null}
           <div className='mt-10 z-10 flex flex-col w-full '>
-            <div className='-mx-8 lg:-mx-0'>
+            {/*       <div className='-mx-8 lg:-mx-0'>
               <Filters
                 filters={agentData?.filters}
                 selectedFilters={selectedFilters}
@@ -116,6 +102,12 @@ const Hero = ({ agentData }: { agentData: AgentData }) => {
             <div className='lg:w-full h-[560px] lg:h-full -mx-8 lg:-mx-0'>
               <AiChat selectedAgent={selectedAgent} />
             </div>
+          </div> */}
+            <Link href='/chat'>
+              <Button variant='secondary' className='w-full md:w-[200px]'>
+                AI CHAT
+              </Button>
+            </Link>
           </div>
           <div className='mt-11 flex items-center justify-center gap-3 md:gap-6 z-10'>
             <Link href='/registry'>
