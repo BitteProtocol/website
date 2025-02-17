820b05517a08e574905c6fbdb3c0ac53375daffd
janis <janis.ellerbrock@gmail.com>
2025-02-17 16:11:34 +0000

    fix z index

--- src/components/layout/SidebarLayout.tsx
+++ src/components/layout/SidebarLayout.tsx
@@ -71,7 +71,7 @@ export default function SidebarLayout({ children }: MainLayoutProps) {
       <AppSidebar />
       <SidebarInset>
         <header
-          className={`flex h-16 shrink-0 items-center gap-2 px-4 sticky top-0 z-[99] ${isScrolled ? 'bg-sidebar' : ''}`}
+          className={`flex h-16 shrink-0 items-center gap-2 px-4 sticky top-0 z-[50] ${isScrolled ? 'bg-sidebar' : ''}`}
         >
           <SidebarTrigger className='-ml-1' />
           <Separator orientation='vertical' className='mr-2 h-4' />
