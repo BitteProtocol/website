478e680b01b92b489427776b1628b8b4b5d334ba
janis <janis.ellerbrock@gmail.com>
2025-02-21 14:23:43 +0000

    change secondary hover

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -15,7 +15,7 @@ const buttonVariants = cva(
         outline:
           'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
         secondary:
-          'bg-secondary text-secondary-foreground hover:bg-white hover:text-mb-gray-550',
+          'bg-secondary text-secondary-foreground hover:bg-secondary-hover hover:text-mb-gray-550',
         card: 'bg-[#414D7D40] text-mb-white-100 hover:text-mb-gray-550 hover:bg-white',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
