7f96fd64c9084f34f8c9afb6644c53e98e9fa3ed
janis <janis.ellerbrock@gmail.com>
2025-03-03 17:31:24 +0000

    first setup for loading

--- src/components/layout/AgentsContent.tsx
+++ src/components/layout/AgentsContent.tsx
@@ -3,6 +3,8 @@
 import dynamic from 'next/dynamic';
 import { useAllAssistants } from '@/hooks/useAssistants';
 import { Skeleton } from '../ui/skeleton';
+import { useState, useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
 
 const AllAgentsWithNoSSR = dynamic(
   () => import('@/components/ui/agents/AllAgents'),
@@ -10,16 +12,24 @@ const AllAgentsWithNoSSR = dynamic(
 );
 
 const AgentContent = () => {
-  const { allAgents: data, loading } = useAllAssistants();
+  const [offset, setOffset] = useState(1);
+  const { allAgents: data, loading } = useAllAssistants(offset, 50);
+  const { ref, inView } = useInView();
 
-  if (loading) {
+  useEffect(() => {
+    if (inView && !loading) {
+      setOffset((prevPage) => prevPage + 1);
+    }
+  }, [inView, loading]);
+
+  /* if (loading) {
     return (
       <div className='flex gap-3'>
         <Skeleton className='w-1/3 h-[70vh]' />
         <Skeleton className='w-2/3 h-[70vh]' />
       </div>
     );
-  }
+  } */
 
   return (
     <div className='relative z-30'>
@@ -28,6 +38,13 @@ const AgentContent = () => {
         filters={data?.filters || []}
         unverifiedAgents={data?.unverifiedAgents || []}
       />
+      {loading && (
+        <div className='flex gap-3'>
+          <Skeleton className='w-1/3 h-[70vh]' />
+          <Skeleton className='w-2/3 h-[70vh]' />
+        </div>
+      )}
+      <div ref={ref} />
     </div>
   );
 };
