8844985fdf0cb3500ae2b90d5f920417c5899667
Ruben Marcus <rubenmarcus.dev@gmail.com>
2025-03-14 11:52:20 +0000

    popup testnet

--- src/lib/providers/Providers.tsx
+++ src/lib/providers/Providers.tsx
@@ -8,10 +8,11 @@ import {
 import { setupMeteorWallet } from '@near-wallet-selector/meteor-wallet';
 
 const BitteWalletSetup = {
-  network: 'mainnet',
+  network: 'testnet',
   callbackUrl: typeof window !== 'undefined' ? window.location.origin : '',
   contractAddress: '',
-  walletUrl: 'https://mintbase-wallet-mfigmohxl-bitteprotocol.vercel.app/',
+  walletUrl:
+    'https://mintbase-wallet-testnet-7ghkautqz-bitteprotocol.vercel.app/',
 };
 
 type ProvidersProps = {
