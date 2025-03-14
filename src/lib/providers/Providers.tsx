25ee0793dec1e73a9aba29497bb37620febe7212
Ruben Marcus <rubenmarcus.dev@gmail.com>
2025-03-14 10:49:34 +0000

    update popup

--- src/lib/providers/Providers.tsx
+++ src/lib/providers/Providers.tsx
@@ -11,7 +11,7 @@ const BitteWalletSetup = {
   network: 'mainnet',
   callbackUrl: typeof window !== 'undefined' ? window.location.origin : '',
   contractAddress: '',
-  walletUrl: 'https://mintbase-wallet-hzimsr3cc-bitteprotocol.vercel.app/',
+  walletUrl: 'https://mintbase-wallet-mfigmohxl-bitteprotocol.vercel.app/',
 };
 
 type ProvidersProps = {
