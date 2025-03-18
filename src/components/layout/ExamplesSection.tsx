62571f61b3edba16b6bbec505a4e08d9d98276cc
Ruben Marcus <ruben@rubenmarcus.dev>
2025-03-18 11:35:39 +0000

    fix cards home (#188)
    also add REAL links to the card.

--- src/components/layout/ExamplesSection.tsx
+++ src/components/layout/ExamplesSection.tsx
@@ -6,6 +6,7 @@ import { useEffect, useRef, useState } from 'react';
 import { Button } from '../ui/button';
 import { Card, CardContent } from '../ui/card';
 import VideoPlayer from '../ui/VideoPlayer';
+import Link from 'next/link';
 
 type BaseCard = {
   id: number;
@@ -33,6 +34,93 @@ type ExampleDataType = {
   btnTitle: string;
   btnUrl: string;
   cards: Card[];
+};
+
+const CardComponent = ({ ...props }) => {
+  const {
+    card,
+    setIsHovered,
+    handleVideoPlay,
+    isHovered,
+    isVideo,
+    isMobile,
+    isVideoPlaying,
+    hasLink,
+  } = props;
+
+  return (
+    <Card
+      key={card?.id}
+      onMouseEnter={() => setIsHovered(card.id)}
+      onMouseLeave={() => setIsHovered(null)}
+      className={
+        !hasLink
+          ? 'relative bg-black border border-[#313E52] w-9/12 md:w-1/2 lg:w-[28%] h-[272px] sm:h-[420px] flex-shrink-0 snap-center hover:border-[#E087FFB2] hover:shadow-custom transition-all duration-300 cursor-pointer ml-4 first:ml-6 last:mr-12 md:first:ml-28 md:last:mr-36 overflow-hidden'
+          : 'flex flex-shrink-0  h-full snap-center'
+      }
+    >
+      <div className={`absolute inset-0 ${isVideo ? 'z-40' : 'z-0'}`}>
+        {isVideo && 'thumbnail' in card ? (
+          <VideoPlayer
+            url={card.bg}
+            thumbnail={card.thumbnail}
+            onPlay={() => handleVideoPlay(card.id)}
+          />
+        ) : (
+          <Image
+            src={card.bg}
+            alt={`background image for ${card.action}`}
+            fill
+            sizes='(max-width: 768px) 75vw, (max-width: 1024px) 50vw, 28vw'
+            style={{
+              objectFit: card.bg.includes('.svg') ? 'contain' : 'cover',
+            }}
+            className='z-10'
+          />
+        )}
+        {card.gradientLayer && (
+          <div className='absolute inset-0 gradient-overlay opacity-50 z-20'></div>
+        )}
+      </div>
+      <CardContent
+        className={`p-[18px] flex flex-col justify-between relative overflow-hidden w-full h-full ${!isVideo ? 'z-30' : ''}`}
+      >
+        <div className='z-50'>
+          <div className='flex justify-between items-center z-50'>
+            <span
+              className={`${fira.className} bg-[#414D7D33] backdrop-blur-md rounded-full text-white uppercase text-xs py-1.5 px-5 self-start`}
+            >
+              {card?.badge}
+            </span>
+            {card?.sub && isMobile && (
+              <p className='text-white text-sm font-semibold'>{card?.sub}</p>
+            )}
+          </div>
+          {isVideo && !isVideoPlaying ? (
+            <div className='max-w-[350px] mt-6'>
+              <span className='text-2xl text-white font-bold'>
+                {card?.action}
+              </span>
+            </div>
+          ) : null}
+        </div>
+        {!isVideo && (
+          <div className='flex justify-center sm:justify-between sm:h-full'>
+            <div
+              className={`sm:self-end min-w-full sm:min-w-[135px] sm:max-w-[300px] px-3 py-2.5 border border-[#313E52] hover:border-none rounded-[10px] flex items-center justify-center ease-out ${isHovered === card.id ? 'bg-white text-mb-gray-550 border-none' : 'bg-[#414D7D40] backdrop-blur-md text-mb-white-100'} transition-all duration-500 ease-in-out`}
+            >
+              <p className='text-sm font-normal'>{card?.action}</p>
+            </div>
+            {card?.sub && !isMobile && (
+              <div className='self-end'>
+                <p className='text-white text-sm font-semibold'>{card?.sub}</p>
+              </div>
+            )}
+          </div>
+        )}
+      </CardContent>
+    </Card>
+  );
 };
 
 export const ExamplesSection = ({
@@ -87,11 +175,6 @@ export const ExamplesSection = ({
     window.open(url, '_blank', 'noopener,noreferrer');
   };
 
-  const goToSmartActions = (link: string, message: string) => {
-    const encodedPrompt = encodeURIComponent(message);
-    window.open(`${link}/${encodedPrompt}`, '_blank');
-  };
-
   useEffect(() => {
     const currentRef = scrollContainerRef.current;
 
@@ -109,6 +192,15 @@ export const ExamplesSection = ({
       currentRef?.removeEventListener('scroll', handleScroll);
     };
   }, []);
+
+  const cardProps = {
+    setIsHovered,
+    handleVideoPlay,
+    isVideo,
+    isMobile,
+    isHovered,
+    isVideoPlaying,
+  };
 
   return (
     <section className='relative w-screen py-20 overflow-hidden'>
@@ -141,86 +233,25 @@ export const ExamplesSection = ({
         ref={scrollContainerRef}
         className='flex overflow-x-scroll overflow-hidden disable-scrollbars relative -mx-6 md:-mx-20 pl-6'
       >
-        {data.cards.map((card) => (
-          <Card
-            className='relative bg-black border border-[#313E52] w-9/12 md:w-1/2 lg:w-[28%] h-[272px] sm:h-[420px] flex-shrink-0 snap-center hover:border-[#E087FFB2] hover:shadow-custom transition-all duration-300 cursor-pointer ml-4 first:ml-6 last:mr-12 md:first:ml-28 md:last:mr-36 overflow-hidden'
-            key={card?.id}
-            onMouseEnter={() => setIsHovered(card.id)}
-            onMouseLeave={() => setIsHovered(null)}
-            onClick={
-              isVideo
-                ? () => {}
-                : card.isSA
-                  ? () => goToSmartActions(card.link, card.action)
-                  : () => handleCardClick(card?.link)
-            }
-          >
-            <div className={`absolute inset-0 ${isVideo ? 'z-40' : 'z-0'}`}>
-              {isVideo && 'thumbnail' in card ? (
-                <VideoPlayer
-                  url={card.bg}
-                  thumbnail={card.thumbnail}
-                  onPlay={() => handleVideoPlay(card.id)}
-                />
-              ) : (
-                <Image
-                  src={card.bg}
-                  alt={`background image for ${card.action}`}
-                  fill
-                  sizes='(max-width: 768px) 75vw, (max-width: 1024px) 50vw, 28vw'
-                  style={{
-                    objectFit: card.bg.includes('.svg') ? 'contain' : 'cover',
-                  }}
-                  className='z-10'
-                />
-              )}
-              {card.gradientLayer && (
-                <div className='absolute inset-0 gradient-overlay opacity-50 z-20'></div>
-              )}
-            </div>
-            <CardContent
-              className={`p-[18px] flex flex-col justify-between relative overflow-hidden w-full h-full ${!isVideo ? 'z-30' : ''}`}
+        {data.cards.map((card) =>
+          isVideo ? (
+            <CardComponent
+              card={card}
+              key={card?.id}
+              {...cardProps}
+              hasLink={false}
+            />
+          ) : (
+            <Link
+              href={card.isSA ? `${card.link}/${card.action}` : `${card.link}`}
+              target='_blank'
+              key={card?.id}
+              className={`relative bg-black border border-[#313E52] w-9/12 md:w-1/2 lg:w-[28%] h-[272px] sm:h-[420px] flex-shrink-0 snap-center hover:border-[#E087FFB2] hover:shadow-custom transition-all duration-300 cursor-pointer ml-4 first:ml-6 last:mr-12 md:first:ml-28 md:last:mr-36 overflow-hidden`}
             >
-              <div className='z-50'>
-                <div className='flex justify-between items-center z-50'>
-                  <span
-                    className={`${fira.className} bg-[#414D7D33] backdrop-blur-md rounded-full text-white uppercase text-xs py-1.5 px-5 self-start`}
-                  >
-                    {card?.badge}
-                  </span>
-                  {card?.sub && isMobile && (
-                    <p className='text-white text-sm font-semibold'>
-                      {card?.sub}
-                    </p>
-                  )}
-                </div>
-                {isVideo && !isVideoPlaying ? (
-                  <div className='max-w-[350px] mt-6'>
-                    <span className='text-2xl text-white font-bold'>
-                      {card?.action}
-                    </span>
-                  </div>
-                ) : null}
-              </div>
-              {!isVideo && (
-                <div className='flex justify-center sm:justify-between sm:h-full'>
-                  <div
-                    className={`sm:self-end min-w-full sm:min-w-[135px] sm:max-w-[300px] px-3 py-2.5 border border-[#313E52] hover:border-none rounded-[10px] flex items-center justify-center ease-out ${isHovered === card.id ? 'bg-white text-mb-gray-550 border-none' : 'bg-[#414D7D40] backdrop-blur-md text-mb-white-100'} transition-all duration-500 ease-in-out`}
-                  >
-                    <p className='text-sm font-normal'>{card?.action}</p>
-                  </div>
-                  {card?.sub && !isMobile && (
-                    <div className='self-end'>
-                      <p className='text-white text-sm font-semibold'>
-                        {card?.sub}
-                      </p>
-                    </div>
-                  )}
-                </div>
-              )}
-            </CardContent>
-          </Card>
-        ))}
+              <CardComponent card={card} {...cardProps} hasLink />
+            </Link>
+          )
+        )}
       </div>
       <div className='mt-8 flex justify-center px-6'>
         <Button
