3ee04aa4ff7e9e8b27799bafbcb0617252ebc833
ruisantiago <ruisantiagomr@gmail.com>
2025-03-19 08:27:35 +0000

    refactor: remove unused and duplicated chain ID entries from network mapping

--- src/lib/utils/chainIds.ts
+++ src/lib/utils/chainIds.ts
@@ -13,12 +13,10 @@ export function getNetworkMapping(
     56: { name: 'BNB', icon: `${baseIconPath}bnb.svg` },
     100: { name: 'Gnosis', icon: `${baseIconPath}gnosis.svg` },
     137: { name: 'Polygon', icon: `${baseIconPath}polygon.svg` },
-    250: { name: 'Avalanche', icon: `${baseIconPath}avax.svg` },
     43114: { name: 'Avalanche', icon: `${baseIconPath}avax.svg` },
     42161: { name: 'Arbitrum', icon: `${baseIconPath}arbi.svg` },
     10: { name: 'Optimism', icon: `${baseIconPath}op.svg` },
     8453: { name: 'Base', icon: `${baseIconPath}base.svg` },
-    11155111: { name: 'Sepolia', icon: `${baseIconPath}eth.svg` },
   };
 }
 
