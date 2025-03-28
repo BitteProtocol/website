1bc16e2628eb67788693a76cf0c2eb24944a94d8
Ruben Marcus <rubenmarcus.dev@gmail.com>
2025-03-28 17:03:25 +0000

    fix console errors on /chat

--- src/app/(dashboard)/chat/page.tsx
+++ src/app/(dashboard)/chat/page.tsx
@@ -1,4 +1,9 @@
-import ChatContent from '@/components/layout/ChatContent';
+import dynamic from 'next/dynamic';
+
+const ChatContent = dynamic(
+  () => import('../../../components/layout/ChatContent'),
+  { ssr: false }
+);
 
 const ChatPage = async () => {
   return <ChatContent />;
