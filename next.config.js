2892aff9e4be41863505683f041b1a8e670ac0d6
ruisantiago <ruisantiagomr@gmail.com>
2025-04-02 12:19:12 +0100

    update Content-Security-Policy to include media sources from Google Cloud

--- next.config.js
+++ next.config.js
@@ -41,7 +41,7 @@ const nextConfig = {
         {
           key: 'Content-Security-Policy',
           value:
-            "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://vercel.live https://fonts.googleapis.com https://substackapi.com https://*.vercel-scripts.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: https: http:; font-src 'self' data: https://fonts.gstatic.com; connect-src 'self' https:; frame-src 'self' https://vercel.live;",
+            "default-src 'self'; media-src 'self' https://storage.cloud.google.com https://*.googleusercontent.com; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://vercel.live https://fonts.googleapis.com https://substackapi.com https://*.vercel-scripts.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: https: http:; font-src 'self' data: https://fonts.gstatic.com; connect-src 'self' https:; frame-src 'self' https://vercel.live;",
         },
       ],
     },
