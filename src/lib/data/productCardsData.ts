e09e93e6c4578c4f15e672848771c3a50b2e17c0
Rui Santiago <ruisantiagomr@gmail.com>
2025-04-02 12:06:36 +0100

    Update product cards data to reflect new features and links - Agent Builder and Chat (#218)

--- src/lib/data/productCardsData.ts
+++ src/lib/data/productCardsData.ts
@@ -1,25 +1,23 @@
-import { MB_URL } from '../url';
-
 export const productCardsData = {
   title: 'EXPLORE OUR CORE PRODUCTS',
   cards: [
     {
       id: 1,
-      badge: 'AI Wallet',
-      title: 'Manage Crypto Assets and Build Transactions with AI',
-      sub: 'No app install, non custodial, passkey powered and sponsored transactions. ',
+      badge: 'Chat',
+      title: 'Execute transactions with natural language',
+      sub: 'Choose between agents from all major blockchains',
       bg: '/examples/brain.webp',
-      btn_text: 'Wallet',
-      link: MB_URL.BITTE_WALLET,
+      btn_text: 'Chat',
+      link: '/chat',
     },
     {
       id: 2,
-      badge: 'Agent Registry',
-      title: 'Agent Registry',
-      sub: 'Fork other agents to make them better or bootstrap your own.',
+      badge: 'Agent Builder',
+      title: 'Build agents with no code',
+      sub: 'Combine existing tools and set multiple parameters for your own agent',
       bg: '/examples/registry.webp',
-      btn_text: 'Agents',
-      link: '/agents',
+      btn_text: 'Agent Builder',
+      link: '/build-agents',
     },
   ],
 };
