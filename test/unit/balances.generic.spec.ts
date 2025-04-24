db7ca2e9ca328c8e4dddd7713fc6510fc9729cd5
Benjamin Smith <bh2smith@users.noreply.github.com>
2025-04-24 10:16:18 +0200

    Add moar tests

--- test/unit/balances.generic.spec.ts
+++ test/unit/balances.generic.spec.ts
@@ -16,28 +16,37 @@ jest.mock('viem', () => {
 });
 
 describe('getBalances', () => {
-  // Using some well-known token addresses on Sepolia
-  // You might want to replace these with tokens you know exist on Sepolia
-  const TEST_TOKENS: Address[] = [
-    '0x1111111111111111111111111111111111111111',
-    '0x2222222222222222222222222222222222222222',
-    '0x3333333333333333333333333333333333333333',
-  ];
-  const TEST_BALANCES = [
-    '0x0000000000000000000000000000000000000000000000000000000123456789',
-    '0xf000000000000000000000000000000000000000000000000de0b6b3a7640000',
-    '0x0000000000000000000000000000000000000000000000000000000000000001',
-  ];
-  mockReadContract.mockResolvedValue([BigInt(8181349), TEST_BALANCES]);
-
   const TEST_WALLET = '0x7f01D9b227593E033bf8d6FC86e634d27aa85568' as Address;
 
   it('should fetch balances for multiple tokens', async () => {
+    const TEST_TOKENS: Address[] = [
+      '0x1111111111111111111111111111111111111111',
+      '0x2222222222222222222222222222222222222222',
+      '0x3333333333333333333333333333333333333333',
+    ];
+    const TEST_BALANCES = [
+      '0x0000000000000000000000000000000000000000000000000000000123456789',
+      '0xf000000000000000000000000000000000000000000000000de0b6b3a7640000',
+      '0x0000000000000000000000000000000000000000000000000000000000000001',
+    ];
+
+    mockReadContract.mockResolvedValue([BigInt(8181349), TEST_BALANCES]);
     const balances = await getBalances(sepolia, TEST_TOKENS, TEST_WALLET);
     expect(balances).toStrictEqual({
       [TEST_TOKENS[0]]: BigInt(TEST_BALANCES[0]).toString(),
       [TEST_TOKENS[1]]: BigInt(TEST_BALANCES[1]).toString(),
       [TEST_TOKENS[2]]: BigInt(TEST_BALANCES[2]).toString(),
     });
+  });
+
+  it('Invalid Token Address', async () => {
+    mockReadContract.mockResolvedValue([BigInt(8181349), ['0x']]);
+    await expect(
+      getBalances(
+        sepolia,
+        ['0x1111111111111111111111111111111111111111'],
+        TEST_WALLET
+      )
+    ).rejects.toThrow('Cannot decode zero data ("0x") with ABI parameters.');
   });
 });
