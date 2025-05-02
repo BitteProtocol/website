e7adb0bd2ac8fab4b3dbef4b7541dbaf7ae31237
Rui Santiago <ruisantiagomr@gmail.com>
2025-05-02 10:23:45 +0100

    Update src/components/layout/SuiWalletConnector.tsx

--- src/components/layout/SuiWalletConnector.tsx
+++ src/components/layout/SuiWalletConnector.tsx
@@ -26,7 +26,6 @@ export const SuiWalletConnector: React.FC<WalletConnectorProps> = ({
   const isWalletInstalled = (walletName: string) => {
     if (!allAvailableWallets) return false;
 
-
     return allAvailableWallets.some(
       (availableWallet) =>
         availableWallet.name.toLowerCase() === walletName.toLowerCase()
